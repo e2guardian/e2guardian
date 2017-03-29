00d30d766c2d6f60f1f685a945308f002ebe1a46
Fredb <numsys@free.fr>
2017-03-29 18:11:40 +0200

    make stats logs (time value) human readable

--- src/FatController.cpp
+++ src/FatController.cpp
@@ -152,8 +152,8 @@ void stat_rec::start()
            fprintf(fs, "time		        httpw	busy	httpwQ	logQ	conx	conx/s	reqs	reqs/s	maxfd	LCcnt\n");
         } else {
            syslog(LOG_ERR, "Unable to open dstats_log %s for writing\nContinuing without logging\n",
-            o.dstat_location.c_str());
-            o.dstat_log_flag = false;
+           o.dstat_location.c_str());
+           o.dstat_log_flag = false;
         };
         maxusedfd = 0;
         fflush(fs);
@@ -176,6 +176,7 @@ void stat_rec::reset()
         start_int = end_int;
     else
         start_int = now;
+
     end_int = start_int + o.dstat_interval;
 
     long cps = cnx / period;
@@ -185,28 +186,12 @@ void stat_rec::reset()
         time( &now);
         timeinfo = localtime ( &now );
         char buffer [50];
-        strftime (buffer,80,"%Y-%m-%d %H:%M",timeinfo);
-    	fprintf(fs, "%s %d	%d	%d	%d	%d	%d	%d	%d	%d	%d\n", buffer, o.http_workers,
-        bc,
-        o.http_worker_Q->size(),
-        o.log_Q->size(),
-        cnx,
-        cps,
-            rqx,
-            rqs,
-        mfd,
-    	LC);
+        strftime (buffer,50,"%Y-%m-%d %H:%M",timeinfo);
+    	fprintf(fs, "%s			%d	%d      %d	%d	%d	%d	%d	%d	%d	%d\n", buffer, o.http_workers,
+        bc, o.http_worker_Q->size(), o.log_Q->size(), cnx, cps, rqx, rqs, mfd, LC);
     } else {
-        fprintf(fs, "%ld	%d	%d      %d      %d      %d      %d      %d      %d      %d      %d      %d\n", now , o.http_workers,
-        bc,
-        o.http_worker_Q->size(),
-        o.log_Q->size(),
-        cnx,
-        cps,
-            rqx,
-            rqs,
-        mfd,
-        LC);
+        fprintf(fs, "%ld        	%d	%d	%d	%d	%d	%d	%d	%d	%d	%d\n", now, o.http_workers,
+        bc, o.http_worker_Q->size(), o.log_Q->size(), cnx, cps, rqx, rqs, mfd, LC);
     }
 
     fflush(fs);
