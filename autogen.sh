bffe43b2cb66c49502e0c3c4f800a7633c76865f
Fredb <numsys@free.fr>
2017-04-13 11:42:18 +0200

    remove unused file

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 set -x
 cp README.md README
-aclocal -I m4 && autoheader && automake --copy && autoconf
+rm missing
+aclocal -I m4 && autoheader && automake --add-missing --copy && autoconf
