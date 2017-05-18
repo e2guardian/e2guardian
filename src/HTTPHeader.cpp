e2guardian/e2guardian

commit ea7599bbc0623ededbeeaecdc0e6f01e4fa1d841

Fix bug causing sites to return a 408 code.

src/HTTPHeader.cpp
@@ -944,47 +944,69 @@ bool HTTPHeader::malformedURL(const String &url)
 
 void HTTPHeader::dbshowheader(String *url, const char *clientip)
 {
+    std::string reqres, inout;
+    if (is_response) {
+        reqres = "RES";
+    } else {
+        reqres = "REQ";
+    }
+    std::hash<std::thread::id> htid;
+//    std::thread::id tid = std::this_thread::get_id();
+    std::size_t tid = htid(std::this_thread::get_id());
+
 
 	if (header.size() != 0){
         String *line;
-        syslog(LOG_INFO, "Client: %s START-------------------------------", clientip);
+        syslog(LOG_INFO, "%d Client: %s START  %s -------------------------------", tid, clientip, reqres.c_str());
         for (std::deque<String>::iterator i = header.begin(); i != header.end(); i++) {
             line = &(*i);
             String line2 = *line;
             bool outgoing = true;
             if (header.front().startsWith("HT")) {
                 outgoing = false;
+                inout = "IN";
+            } else {
+                inout = "OUT";
             }
-            if (outgoing)
-                syslog(LOG_INFO, "OUT: Client IP at %s header: %s", clientip, line2.c_str());
-            if (!outgoing)
-                syslog(LOG_INFO, "IN: Client IP at %s header: %s", clientip, line2.c_str());
+                syslog(LOG_INFO, "%d: %s: Client IP at %s header: %s", tid, inout.c_str(), clientip, line2.c_str());
             }
-            syslog(LOG_INFO, "Client: %s END-------------------------------", clientip);
+            syslog(LOG_INFO, "%d: Client: %s END %s -------------------------------", tid, clientip, reqres.c_str());
     } else {
-            syslog(LOG_INFO, "Client: %s Call to dbshowheader but header is empty", clientip);
+            syslog(LOG_INFO, "%d: Client: %s Call to dbshowheader but %s header is empty", tid, clientip,reqres);
 #ifdef DGDEBUG
-            std::cout << "Call : from HTTPHeader.cpp to dbshowheader but header is empty" << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
+            std::cout << tid << "Call : from HTTPHeader.cpp to dbshowheader but" << reqres << " header is empty" << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
 #endif
     }
 }
 
 void HTTPHeader::dbshowheader(bool outgoing)
 {
+    std::string reqres, inout;
+    if (is_response) {
+        reqres = "RES";
+    } else {
+        reqres = "REQ";
+    }
+    if (outgoing)
+        inout = reqres + "OUT";
+    else
+        inout = reqres + "IN";
+
+    std::hash<std::thread::id> htid;
+//    std::thread::id tid = std::this_thread::get_id();
+    std::size_t tid = htid(std::this_thread::get_id());
+
     if (header.size() != 0){
         String *line;
         for (std::deque<String>::iterator i = header.begin(); i != header.end(); i++) {
             line = &(*i);
             String line2 = *line;
-            if (outgoing)
-                syslog(LOG_INFO, "OUT: dbshowheader bool - header: %s", line2.c_str());
-            if (!outgoing)
-                syslog(LOG_INFO, "IN: dbshowheader bool - header: %s", line2.c_str());
+                syslog(LOG_INFO, "%d:%s: dbshowheader bool - header: %s", tid, inout.c_str(), line2.c_str());
         }
     } else {
-            syslog(LOG_INFO, "Call : from HTTPHeader.cpp to dbshowheader but header is empty");
+            syslog(LOG_INFO, "%d:Call : from HTTPHeader.cpp to dbshowheader but header is empty", tid);
 #ifdef DGDEBUG
-            std::cout << "Call : from HTTPHeader.cpp to dbshowheader but header is empty" << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
+            std::cout <<  tid << "Call : from HTTPHeader.cpp to dbshowheader but header is empty" << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
 #endif
     }
 }
@@ -1934,7 +1956,7 @@ bool HTTPHeader::out(Socket *peersock, Socket *sock, int sendflag, bool reconnec
                 l += (*i) + "\n";
         }
     }
-    l += "\n";
+    l += "\r\n";
 
 #ifdef DGDEBUG
     std::cout << "headertoclient:" << l << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;
@@ -2118,7 +2140,7 @@ bool HTTPHeader::in(Socket *sock, bool allowpersistent, bool honour_reloadconfig
     	return false;
     }
 #ifdef DGDEBUG
-    dbshowheader(false);
+//    dbshowheader(false);
 #endif
 
     header.pop_back(); // remove the final blank line of a header

src/Socket.cpp
@@ -807,13 +807,17 @@ int Socket::getLine(char *buff, int size, int timeout, bool honour_reloadconfig,
     if (!isssl) {
         return BaseSocket::getLine(buff, size, timeout, honour_reloadconfig, chopped, truncated);
     }
+#ifdef DGDEBUG
+		std::cout << "in getline (SSL): timeout: " << timeout << " size: " << size << std::endl;
+
+#endif
 
     // first, return what's left from the previous buffer read, if anything
     int i = 0;
     if ((bufflen - buffstart) > 0) {
-        /*#ifdef DGDEBUG
-		std::cout << "data already in buffer; bufflen: " << bufflen << " buffstart: " << buffstart << std::endl;
-#endif*/
+#ifdef DGDEBUG
+		std::cout << "getline(ssl) data already in buffer; bufflen: " << bufflen << " buffstart: " << buffstart << std::endl;
+#endif
         int tocopy = size - 1;
         if ((bufflen - buffstart) < tocopy)
             tocopy = bufflen - buffstart;
@@ -838,10 +842,10 @@ int Socket::getLine(char *buff, int size, int timeout, bool honour_reloadconfig,
  //       } catch (std::exception &e) {
 //            throw std::runtime_error(std::string("Can't read from socket: ") + strerror(errno)); // on error
  //       }
-        if( bcheckSForInput(timeout))
+ //       if( bcheckSForInput(timeout))
             bufflen = SSL_read(ssl, buffer, 4096);
 #ifdef DGDEBUG
-//std::cout << "read into buffer; bufflen: " << bufflen <<std::endl;
+std::cout << "getline (SSL) read into buffer; bufflen: " << bufflen <<std::endl;
 #endif
         if (bufflen < 0) {
   //          if (errno == EINTR ) {
@@ -862,7 +866,7 @@ int Socket::getLine(char *buff, int size, int timeout, bool honour_reloadconfig,
                 *truncated = true;
 #ifdef DGDEBUG
             if (truncated)
-            	std::cout << "Getline truncated buffer end reached before we found a newline: bufflen == 0" << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
+            	std::cout << "Getline(SSL) truncated buffer end reached before we found a newline: bufflen == 0" << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
 #endif
             return i;
         }
@@ -880,7 +884,7 @@ int Socket::getLine(char *buff, int size, int timeout, bool honour_reloadconfig,
         }
         i += tocopy;
 #ifdef DGDEBUG
-            std::cout << "buffer =: " << buff[i] << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;
+            std::cout << "getLine(SSL) buffer =: " << buff[i] << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;
 #endif
     }
     // oh dear - buffer end reached before we found a newline
@@ -889,7 +893,7 @@ int Socket::getLine(char *buff, int size, int timeout, bool honour_reloadconfig,
         *truncated = true;
 #ifdef DGDEBUG
     	if (truncated)
-            std::cout << "Getline truncated buffer end reached before we found a newline: " << buff  << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
+            std::cout << "Getline(SSL) truncated buffer end reached before we found a newline: " << buff  << " Line: " << __LINE__ << " Function: " << __func__ << std::endl;;
 #endif
     return i;
 }
@@ -974,7 +978,7 @@ int Socket::readFromSocketn(char *buff, int len, unsigned int flags, int timeout
 
     while (cnt > 0) {
     //    try {
-            bcheckSForInput(timeout);        //  this may be wrong - why is data not being read into socket buffer????
+    //        bcheckSForInput(timeout);        //  this may be wrong - why is data not being read into socket buffer????
     //    } catch (std::exception &e) {
      //       return -1;
      //   }
@@ -1033,23 +1037,23 @@ int Socket::readFromSocket(char *buff, int len, unsigned int flags, int timeout,
 #endif
     int rc;
     while (cnt > 0) {
-    if (check_first) {
-#ifdef DGDEBUG
-        std::cout << "Socket::readFromSocket(ssl): before bcheckSForInput timeout is: " << timeout  << std::endl;
-#endif
+ //   if (check_first) {
+//#ifdef DGDEBUG
+//       std::cout << "Socket::readFromSocket(ssl): before bcheckSForInput timeout is: " << timeout  << std::endl;
+//#endif
 //          if(!bcheckSForInput(timeout))
   //          return -1;
-#ifdef DGDEBUG
-        std::cout << "Socket::readFromSocket(ssl): after bcheckSForInput OK return: " << std::endl;
-#endif
-   }
+//#ifdef DGDEBUG
+//        std::cout << "Socket::readFromSocket(ssl): after bcheckSForInput OK return: " << std::endl;
+//#endif
+  // }
 //    while (true)
         bool inbuffer;
 #ifdef DGDEBUG
         std::cout << "Socket::readFromSocket(ssl): before SSL_read: "  << std::endl;
 #endif
         ERR_clear_error();
-        if (cnt > 4095) {
+       if (true) {     // was if(cnt > 4095)
             inbuffer = false;
            rc = SSL_read(ssl, buff, cnt);        //  data larger than SSL buffer so ok to read directly into output buffer
         } else {
@@ -1086,7 +1090,7 @@ int Socket::readFromSocket(char *buff, int len, unsigned int flags, int timeout,
               buffstart += tocopy;
               buff += tocopy;
 #ifdef DGDEBUG
-        std::cout << "Inbuffer SSL read to returned " << tocopy << " bytes" << std::endl;
+        std::cout << "Inbuffer SSL read returned " << tocopy << " bytes" << std::endl;
 #endif
            }
          } else {

commit 0279ef9f62a397d5c39352e5dae72c7331d26c5f

Fix  header reading bug introduced since ea7599bbc

src/HTTPHeader.cpp
@@ -2174,7 +2174,7 @@ bool HTTPHeader::in(Socket *sock, bool allowpersistent, bool honour_reloadconfig
         return false;
     }
 
-    checkheader(allowpersistent); // sort out a few bits in the header
     header.pop_back(); // remove the final blank line of a header
+    checkheader(allowpersistent); // sort out a few bits in the header
     return true;
 }