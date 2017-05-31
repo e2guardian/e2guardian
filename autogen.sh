1f4cdf697c8037c338ae9284b80a90df38423a28
Fredb <numsys@free.fr>
2017-05-31 09:36:34 +0200

    Polish autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -x
-cp README.md README
-rm missing
+cp README.md README > /dev/null 2>&1 
+rm missing > /dev/null 2>&1 
 aclocal -I m4 && autoheader && automake --add-missing --copy && autoconf
