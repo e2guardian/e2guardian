459069a96bc88e67a1cd05ff4682e7ecb7c815b2
Fredb <numsys@free.fr>
2017-11-27 15:15:56 +0100

    Fix Bug #302

--- src/FatController.cpp
+++ src/FatController.cpp
@@ -1056,7 +1056,7 @@ void log_listener(std::string log_location, bool logconerror, bool logsyslog) {
                 hier += o.proxy_ip;
 
                 builtline =
-                        utime + " " + duration + " " + ((clienthost.length() > 0) ? clienthost : from) + " " + hitmiss +
+                        utime + " " + duration + " " + from + " " + ((clienthost.length() > 0) ? clienthost : from) + " " + hitmiss +
                         " " + ssize + " "
                         + how + " " + where + " " + who + " " + hier + " " + mimetype;
                 break;
