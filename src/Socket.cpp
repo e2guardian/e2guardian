abeb0a1fa12c5a077b94620b66605790de8a1272
Philip Pearce <philip.pearce@e2bn.org>
2018-03-05 16:55:50 +0000

    Fix double 0 chunks on loopChunk

--- src/Socket.cpp
+++ src/Socket.cpp
@@ -1200,7 +1200,22 @@ int Socket::loopChunk(int timeout)    // reads chunks and sends back until 0 len
     int csize = 1;
     while (csize > 0) {
         csize = readChunk(buff,32000, timeout);
-        if (!(csize > -1 && writeChunk(buff,csize,timeout))) {
+        if (csize == 0)     // end chunk
+        {
+            if (!writeChunkTrailer(chunked_trailer))
+            {
+#ifdef CHUNKDEBUG
+                std::cerr << thread_id << "loopChunk - error in writing chunk trailer" << std::endl;
+#endif
+                return -1;
+
+            };
+#ifdef CHUNKDEBUG
+            std::cerr << thread_id << "loopChunk  tot_size=" << tot_size << std::endl;
+#endif
+            return tot_size;
+        }
+        if (!(csize > 0 && writeChunk(buff,csize,timeout))) {
 #ifdef CHUNKDEBUG
             std::cerr << thread_id << "loopChunk - error" << std::endl;
 #endif
@@ -1208,11 +1223,6 @@ int Socket::loopChunk(int timeout)    // reads chunks and sends back until 0 len
         }
         tot_size += csize;
     }
-    writeChunkTrailer(chunked_trailer);
-#ifdef CHUNKDEBUG
-    std::cerr << thread_id << "loopChunk  tot_size=" << tot_size << std::endl;
-#endif
-    return tot_size;
 }
 
 
