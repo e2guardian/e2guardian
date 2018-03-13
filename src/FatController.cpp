bb55c0a54cd7b36855dcbcf6a9bddba198674388
Fredb <numsys@free.fr>
2018-03-13 14:57:28 +0100

    Fix bug #354 and polish log

--- src/FatController.cpp
+++ src/FatController.cpp
@@ -974,36 +974,14 @@ void log_listener(std::string log_location, bool logconerror, bool logsyslog) {
 		}
 
 		if (o.log_file_format != 3) {
-		    // "when" not used in format 3, and not if logging timestamps instead
-		    String temp;
-//		    time_t tnow; // to hold the result from time()
-//		    struct tm *tmnow; // to hold the result from localtime()
-		    time_t now = time(NULL);
-		    char date[32];
-		    struct tm * tm = localtime(&now);
-//		    time(&tnow); // get the time after the lock so all entries in order
-//		    tmnow = localtime(&tnow); // convert to local time (BST, etc)
-		    strftime(date, sizeof date, "%y.%m.%d %H:%M:%S", tm);
-		    
-/*		    year = String(tmnow->tm_year + 1900);
-		    month = String(tmnow->tm_mon + 1);
-		    day = String(tmnow->tm_mday);
-		    hour = String(tmnow->tm_hour);
-		    temp = String(tmnow->tm_min);
-		    if (temp.length() == 1) {
-			temp = "0" + temp;
-		    }
-		    min = temp;
-		    temp = String(tmnow->tm_sec);
-		    if (temp.length() == 1) {
-			temp = "0" + temp;
-		    }
-		    sec = temp;
-		    when = year + "." + month + "." + day + " " + hour + ":" + min + ":" + sec;
-*/
-		    when = date;
-		    // append timestamp if desired
-		    if (o.log_timestamp)
+		// "when" not used in format 3, and not if logging timestamps instead
+		  time_t now = time(NULL);
+		   char date[32];
+		  struct tm * tm = localtime(&now);
+		  strftime(date, sizeof date, "%y.%m.%d %H:%M:%S", tm);
+		   when = date;
+		   // append timestamp if desired
+		   if (o.log_timestamp)
 			when += " " + utime;
 		}
 
