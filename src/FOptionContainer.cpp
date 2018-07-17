56cfde0f4defead6d8e6501baa1ad2518229a278
Philip Pearce <philip.pearce@e2bn.org>
2018-07-17 10:12:31 +0100

    Extra fix to #411

--- src/FOptionContainer.cpp
+++ src/FOptionContainer.cpp
@@ -72,27 +72,35 @@ FOptionContainer::~FOptionContainer()
 void FOptionContainer::reset()
 {
     conffile.clear();
-    delete banned_page;
-    banned_page = NULL;
+    if (neterr_page != nullptr)
+    {
+        delete neterr_page;
+        neterr_page = nullptr;
+    }
+    if (banned_page != nullptr) {
+        delete banned_page;
+        banned_page = nullptr;
+    }
     resetJustListData();
 }
 
-void FOptionContainer::resetJustListData()
-{
-    if (!(group_mode == 1))
-        return;
-    if (banned_phrase_flag)
-        o.lm.deRefList(banned_phrase_list);
+    void FOptionContainer::resetJustListData()
+    {
+        if (!(group_mode == 1))
+            return;
+        if (banned_phrase_flag)
+            o.lm.deRefList(banned_phrase_list);
 
-    banned_phrase_flag = false;
-    content_regexp_flag = false;
-    ssl_mitm = false;
+        banned_phrase_flag = false;
+        content_regexp_flag = false;
+        ssl_mitm = false;
 
-    banned_phrase_list_index.clear();
+        banned_phrase_list_index.clear();
 
-    //	conffile.clear();
+        //	conffile.clear();
 
-    content_regexp_list_comp.clear();
+        content_regexp_list_comp.clear();
+    }
     content_regexp_list_rep.clear();
 }
 
