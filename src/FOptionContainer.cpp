5b2e146e3b7c7bff6f147704dd9bf7db00f429db
fredbcode <numsys@free.fr>
2018-09-04 08:55:18 +0000

    Fix bug #432 Block html page gets shown twice

--- src/FOptionContainer.cpp
+++ src/FOptionContainer.cpp
@@ -473,7 +473,7 @@ bool FOptionContainer::read(const char *filename) {
             } else {  // if blank will default to HTML template file
                 neterr_template = o.languagepath + "template.html";
                 neterr_page = new HTMLTemplate;
-                if (!(banned_page->readTemplateFile(neterr_template.toCharArray()))) {
+                if (!(neterr_page->readTemplateFile(neterr_template.toCharArray()))) {
                     if (!is_daemonised) {
                         std::cerr << thread_id << "Error reading default HTML and NetErr Template file: " << html_template << std::endl;
                     }
