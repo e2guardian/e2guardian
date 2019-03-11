0cf347ba631baf1df172e441bdb3234a5dc2e40b
Philip Pearce <philip.pearce@e2bn.org>
2019-03-11 10:38:52 +0000

    Possible fix for #508

--- src/ConnectionHandler.cpp
+++ src/ConnectionHandler.cpp
@@ -1216,7 +1216,8 @@ int ConnectionHandler::handleConnection(Socket &peerconn, String &ip, bool ismit
                             // treat connect like normal get
                             checkme.isconnect = false;
                             checkme.isexception = true;
-                        } else if (!authed && o.auth_needs_proxy_in_plugin && !checkme.isexception) {
+                        } else if (!authed && checkme.request_header->isProxyRequest
+                                   && o.auth_needs_proxy_in_plugin && !checkme.isexception) {
                             checkme.isItNaughty = true;
                             checkme.message_no =  110;
                         }
