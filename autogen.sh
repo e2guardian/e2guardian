cd33d1c91c6c4403ed4fea37730f66dff8fde812
root <numsys@free.fr>
2020-02-27 12:02:48 +0100

    Oups: restore automake --add-missing

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,4 @@ set -x
 cp README.md README
 # remove previous generation
 rm -f compile config.guess config.sub missing depcomp
-aclocal -I m4 && autoheader && automake --copy && autoconf
+aclocal -I m4 && autoheader && automake --add-missing --copy && autoconf
