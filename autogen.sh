565b9212a8c472b988041e594f032a51688e1fe0
root <numsys@free.fr>
2020-02-27 11:58:19 +0100

    Remove previous compilation autogen

--- autogen.sh
+++ autogen.sh
@@ -1,4 +1,6 @@
 #! /bin/sh
 set -x
 cp README.md README
-aclocal -I m4 && autoheader && automake --add-missing --copy && autoconf
+# remove previous generation
+rm -f compile config.guess config.sub missing depcomp
+aclocal -I m4 && autoheader && automake --copy && autoconf
