59d6400fa7015437332c20a93cd3492c317f63d4
Philip Pearce <philip.pearce@e2bn.org>
2023-07-03 12:17:26 +0100

    staged so as to work on other area

--- src/LogFormat.hpp
+++ src/LogFormat.hpp
@@ -20,44 +20,47 @@ class LogFormat {
     bool add_header = false;
     bool use_dash_for_blanks = false;
     enum F {
-        blank,
-        endutime,      // unix time in secs since 1/1/1970
-        endltime,      // local time in human-readable format
+        blank,          // empty column
+        end_utime,      // end timestamp unix time in secs since 1/1/1970 - was 'when' in log_listener
+        end_ltime,      // end timestamp local time in human-readable format
         server,
         user,      // was 'who' in log_listener
         clientip,  // was 'from' in log_listener
         clientHost,
-        clientHostOrIp,
-        where,   // = full url
-        rtype,        // request type ('GET' etc) was 'how'
-        rcode,   // from response ???
-        ssize,
+        clientHostOrIp,  // client host (from reverse DNS) if unavailable the client IP
+        url,   // = full url - was 'where'
+        rqtype,        // request type ('GET' etc) was 'how'
+        rscode,   //      response code
+        ssize,      // size of body in bytes
         mimetype,
         useragent,  // replace with header spec???
-        durationms, // time taken to service request
-        message_no,
-        what,       // actionWord(s) + what_is_naughty
+        durationms, // time taken to service request in ms (= end_utime - start_utime)
+        message_no, // e2g message_no or 200 if OK
+        whatCombi,       // actionWord(s) + what_is_naughty - was 'what'
+        actionWord,     // actionWord(s) *DENIED*, *EXCEPTION, etc
+        whats_naughty,  //
         mimetype,
         naughtiness,     // was sweight
         category,
         groupName,
         groupNo,
         searchTerms,
-        start_utime,  // tv_sec
-        start_ltime,  // tv_sec
+        start_utime,  // start timestamp unix time in secs since 1/1/1970
+        start_ltime,    // start timestamp local time in human-readable format
         extflags,
         params,   // ??  does not seem to do anything - remove
-        logid_1,  // ?? Only used in log type 4
-        logid_2,  // ?? Only used in log type 4
+        logid_1,  //  Only used in log type 4 - but keep
+        logid_2,  //  Only used in log type 4 - keep
         postdata,  // ??  Not implimented in v5 so far
         proxyIp,       // Upstream proxy IP - was 'heir'
         reqh,    // request header  - not sure if needed here as special case
         resh,    // response header  - not sure if needed here as special case
     };
     struct item {
-        F code = blank;
-        String name = "";
-        String header_name = "";
+        F code = blank;  // enum
+        String name = ""; // name of item for most, but 'item:header_id:' for reqh and resh
+                // this will be used in header log if add_header is true
+        String header_name = ""; // HTTP header id i.e. 'header_id:'
     };
     std::deque<item> item_list;
 };
